8de8457a41cbdaa5cb7a9dac2481e24de3e38952
RedDotz20 <carlos_tabangay@outlook.com>
2023-10-07 17:04:15 +0800

    feat: login base component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 // import { Button } from '@nextui-org/react';
+import Login from './pages/Login';
 
 export default function App() {
   return (
-    <main className="flex flex-col h-screen w-full justify-center items-center">
-      <section className="">Online Art App</section>
+    <main className="flex flex-col h-screen w-full justify-center items-center bg-[#3b3b3b]">
+      <Login />
     </main>
   );
 }
