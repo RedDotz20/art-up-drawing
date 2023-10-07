5116a4e4917b0fc5e51d0d60cc2eae4c1c5eb788
RedDotz20 <carlos_tabangay@outlook.com>
2023-10-07 16:17:54 +0800

    proj init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// import { Button } from '@nextui-org/react';
+
+export default function App() {
+  return (
+    <main className="flex flex-col h-screen w-full justify-center items-center">
+      <section className="">Online Art App</section>
+    </main>
+  );
+}
